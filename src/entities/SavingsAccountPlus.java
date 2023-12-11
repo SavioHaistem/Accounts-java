4e8a7068d4137792f28348bcf5d46c3056dd3378
SavioHaistem <savio.dantas.5858@gmail.com>
2023-12-11 15:11:27 -0300

    Override method

--- src/entities/SavingsAccountPlus.java
+++ src/entities/SavingsAccountPlus.java
@@ -1,5 +1,17 @@
 package entities;
 
-public class SavingsAccountPlus {
+public class SavingsAccountPlus extends SavingsAccount {
+    private Double bonus;
+
+    public SavingsAccountPlus(Integer id, String holder, Double balance, Double interestRate) {
+        super(id, holder, balance, interestRate);
+    }
+
+/*
+    @Override
+    public void withdraw(double mount) {
+        
+    }
+*/
 
 }
